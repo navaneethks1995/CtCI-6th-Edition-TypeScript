25553488a4d28d2296f99c4ea3e736267de7aacb
unknown <navaneethks1995@gmail.com>
2023-05-09 17:41:29 +0530

    add more test case

--- arrays_and_strings/isUnique.test.ts
+++ arrays_and_strings/isUnique.test.ts
@@ -2,4 +2,10 @@ import isUnique from "./isUnique";
 
 test('should return true for non repeating character strings', () => {
   expect(isUnique('abcdefghij')).toBe(true);
+  expect(isUnique('')).toBe(true);
+})
+
+test('should return false for repeating character strings', () => {
+  expect(isUnique('abcdbefghij')).toBe(false);
+  expect(isUnique('aaaaaaaaa')).toBe(false);
 })
